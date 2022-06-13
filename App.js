63ba81c480dd9c52985fc0306c074f32e05d2a86
Muhammad Ahmed <ahmed495802@gmail.com>
2022-06-13 07:13:07 -0700

    Added Patient Search Screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Text, View, TouchableOpacity, FlatList, TextInput} from 'react-native';
+import {Text, Image, View, TouchableOpacity, FlatList, TextInput} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
@@ -7,10 +7,10 @@ export default function App() {
   // const {portfolio} = route.params;
   // const data = portfolio.skills;
   const data = [
-    {id:1, name:'ahmed'},
-    {id:2, name:'faiq'},
-    {id:3, name:'ali'},
-    {id:4, name:'shahzad'}
+    {id:1, name:'ahmed', img:'https://www.w3schools.com/w3images/avatar6.png'},
+    {id:2, name:'faiq', img:'https://www.w3schools.com/w3images/avatar6.png'},
+    {id:3, name:'ali', img:'https://www.w3schools.com/w3images/avatar6.png'},
+    {id:4, name:'shahzad', img:'https://www.w3schools.com/w3images/avatar6.png'}
 
 ];
 
@@ -29,25 +29,36 @@ export default function App() {
         paddingLeft: 20,
         paddingRight: 20,
       }}>
-        <View style={{flexDirection:'row', backgroundColor:'black',justifyContent:'space-between', alignItems:'center',borderRadius:30, paddingHorizontal:25}}>
-          <TextInput placeholder='Enter Patient Id' />
-          <Icon name="search" size={20} color="white" style={{}}/>
+        <Text style={{fontSize:32, textAlign:'center', color:'rgba(12,25,150,0.8)', fontWeight:'bold', marginVertical:25}}>Request Patient Access</Text>
+        <View style={{flexDirection:'row', marginTop:10, backgroundColor:'rgba(0,0,0,0.6)',justifyContent:'space-between', alignItems:'center',borderRadius:30, paddingHorizontal:25}}>
+          <TextInput placeholder='Enter Patient Id' style={{width:'90%'}} />
+          <Icon name="search" size={20} color="white" style={{padding:10}}/>
         </View>
         <FlatList
+        style={{marginTop:35}}
         data={data}
         renderItem={({ item }) => (
   
            <View key={item.id} style={{flex:1,
-              justifyContent: 'center',
+              flexDirection:'row',
+              justifyContent: 'space-around',
               alignItems: 'center',
-              height: 100,
+              borderRadius:35,
+              height: 85,
               margin: 3,
-              backgroundColor: generateColor()}}>
-              <Text style= {{fontSize:18, fontWeight:"bold", color:"white"}}>{item.name}</Text>
+              backgroundColor: 'rgba(12,25,150,0.6)'}}>
+              <View style={{flexDirection:'row', alignItems: 'center', minWidth:150}}>
+              <Image
+                style={{width: 50,height: 50, borderRadius:50}}
+                source={{uri:item.img}}
+              />
+              <Text style= {{fontSize:18, fontWeight:"bold", color:"white", marginLeft:10}}>{item.id}. {item.name}</Text>
+              </View>
+              <TouchableOpacity style={{backgroundColor:'rgba(0,0,0,0.6)', padding:15, borderRadius:25}}><Text style={{fontWeight:'bold'}}>Request Access</Text></TouchableOpacity>
             </View>
         )}
         //Setting the number of column
-        numColumns={3}
+        numColumns={1}
         keyExtractor={(item, index) => index}
       />
       
