328928dabb61ab63401d8a26602632d0f787047f
Muhammad Ahmed <ahmed495802@gmail.com>
2022-06-13 06:42:12 -0700

    intalled icons

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {Text, View, TouchableOpacity, FlatList, TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default function App() {
   // const {portfolio} = route.params;
@@ -27,6 +29,27 @@ export default function App() {
         paddingLeft: 20,
         paddingRight: 20,
       }}>
+        <View style={{flexDirection:'row', backgroundColor:'black',justifyContent:'space-between', alignItems:'center',borderRadius:30, paddingHorizontal:25}}>
+          <TextInput placeholder='Enter Patient Id' />
+          <Icon name="search" size={20} color="white" style={{}}/>
+        </View>
+        <FlatList
+        data={data}
+        renderItem={({ item }) => (
+  
+           <View key={item.id} style={{flex:1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 100,
+              margin: 3,
+              backgroundColor: generateColor()}}>
+              <Text style= {{fontSize:18, fontWeight:"bold", color:"white"}}>{item.name}</Text>
+            </View>
+        )}
+        //Setting the number of column
+        numColumns={3}
+        keyExtractor={(item, index) => index}
+      />
       
     </View>
   );
