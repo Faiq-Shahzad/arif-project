5bca993f50b48c5f1fc93e0940a29981d1a75b33
Faiq Shahzad <f03315558407@gmail.com>
2022-06-14 22:31:15 +0500

    Creating UseStates

--- components/signup.js
+++ components/signup.js
@@ -6,5 +6,11 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
 
+  const [signup_name, setSignUpName] = useState("")
+  const [signup_password, setSignUpPassword] = useState("")
+  const [signup_confirm, setSignUpConfirm] = useState("")
+  const [date, setDate] = useState("2022-06-15")
+  const [value, setValue] = React.useState('male');
+
 
 }
