9515070ab9d2d650412e1b3f68ee02e44e748fdf
Faiq Shahzad <f03315558407@gmail.com>
2022-06-14 22:28:25 +0500

    Importing Neccessary Components

---
+++ components/signup.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import { Text, View, FlatList, Alert, TextInput, TouchableOpacity, ScrollView} from 'react-native';
+import {Button, RadioButton} from 'react-native-paper';
+// import DatePicker from 'react-native-datepicker';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function App() {
+
+
+}
