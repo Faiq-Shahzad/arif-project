30ecd2016d8e4d4705521fc1ab332b3b72567f77
Faiq Shahzad <f03315558407@gmail.com>
2022-06-18 00:26:59 +0500

    Date-Picker Solved

--- App.js
+++ App.js
@@ -1,56 +1,17 @@
 import React, {useState} from 'react';
 import {Text, View, TouchableOpacity, FlatList, TextInput} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import SignUp from './components/signup.js'
+
 
 
 export default function App() {
-  // const {portfolio} = route.params;
-  // const data = portfolio.skills;
-  const data = [
-    {id:1, name:'ahmed'},
-    {id:2, name:'faiq'},
-    {id:3, name:'ali'},
-    {id:4, name:'shahzad'}
 
-];
-
-  const generateColor = () => {
-    const randomColor = Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, '0');
-    return `#${randomColor}`;
-  };
   return (
-    <View
-      style={{
-        backgroundColor: 'white',
-        flex: 1,
-        paddingTop: 10,
-        paddingLeft: 20,
-        paddingRight: 20,
-      }}>
-        <View style={{flexDirection:'row', backgroundColor:'black',justifyContent:'space-between', alignItems:'center',borderRadius:30, paddingHorizontal:25}}>
-          <TextInput placeholder='Enter Patient Id' />
-          <Icon name="search" size={20} color="white" style={{}}/>
-        </View>
-        <FlatList
-        data={data}
-        renderItem={({ item }) => (
-  
-           <View key={item.id} style={{flex:1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 100,
-              margin: 3,
-              backgroundColor: generateColor()}}>
-              <Text style= {{fontSize:18, fontWeight:"bold", color:"white"}}>{item.name}</Text>
-            </View>
-        )}
-        //Setting the number of column
-        numColumns={3}
-        keyExtractor={(item, index) => index}
-      />
+    <View>
       
+      <Icon name="login" size={20} color="black" style={{}}/>
+            
     </View>
   );
 }
--- components/signup.js
+++ components/signup.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import { Text, View, FlatList, Alert, TextInput, TouchableOpacity, ScrollView} from 'react-native';
 import {Button, RadioButton} from 'react-native-paper';
-// import DatePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import DatePicker from 'react-native-datepicker';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function App() {
 
@@ -17,7 +17,7 @@ export default function App() {
 
       <View style={{backgroundColor:"dodgerblue", width:"100%", alignItems:"center", padding:10, paddingTop:40}}>
         <Text style={{fontSize:25, fontWeight:"bold", color:"white"}}>Sign Up</Text>
-        <Icon name="sign-up" size={20} color="white" style={{}}/>
+        <Icon name="person_add" size={20} color="white" style={{}}/>
       </View>
 
 
@@ -39,8 +39,8 @@ export default function App() {
           <TextInput placeholder="e.g: 0333-5558444" style={{marginTop:5, borderColor:"lightgrey", borderRadius:5, borderWidth:1, padding:3}}></TextInput>
           <Text style={{marginTop:15}}>Date of Birth:</Text>
           
-          {/* <DatePicker style={{width: "100%"}} date={date} mode="date" placeholder="select date" format="YYYY-MM-DD" minDate="2022-05-01" maxDate="2023-06-01" confirmBtnText="Confirm"
-            cancelBtnText="Cancel" customStyles={{ dateIcon: {position: 'absolute', left: 0, top: 4, marginLeft: 0}, dateInput: { marginLeft: 36, borderRadius:5, borderColor:"lightgrey"} }} /> */}
+          <DatePicker style={{width: "100%"}} date={date} mode="date" placeholder="select date" format="YYYY-MM-DD" minDate="2022-05-01" maxDate="2023-06-01" confirmBtnText="Confirm"
+            cancelBtnText="Cancel" customStyles={{ dateIcon: {position: 'absolute', left: 0, top: 4, marginLeft: 0}, dateInput: { marginLeft: 36, borderRadius:5, borderColor:"lightgrey"} }} />
 
           <Text style={{marginTop:15}}>Gender:</Text>
 
